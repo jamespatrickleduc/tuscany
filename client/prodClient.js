331d983157863d7fa68b63418373300d9b2a4ac8
Patrick LeDuc <jamespatrickleduc@gmail.com>
2021-01-15 10:05:54 -0500

    added docker

--- client/prodClient.js
+++ client/prodClient.js
@@ -1,13 +1,11 @@
 const express = require("express");
-const fs = require("fs");
 const app = express();
 const path = require("path");
 
 app.use(express.static("dist"));
 
-// viewed at http://localhost:8080
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
-app.listen(80);
+app.listen(3000);
--- server/server.js
+++ server/server.js
@@ -8,10 +8,24 @@
 
 import { Server } from "boardgame.io/server";
 //import { Server } from "boardgame";
-import Tuscany from "./src/tuscany/game";
+import Tuscany from "../client/src/tuscany/game";
 
 const PORT = process.env.PORT || 8000;
 const server = Server({ games: [Tuscany] });
 server.run(PORT, () => {
   console.log(`Serving at: http://localhost:${PORT}/`);
 });
+
+/*
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static("dist"));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
+});
+
+app.listen(3000);
+*/
