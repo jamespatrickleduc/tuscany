a42fbf495436546374c74f8338ae3674c60f53eb
Patrick LeDuc <jamespatrickleduc@gmail.com>
2021-01-20 11:06:51 -0500

    this might have worked

--- client/src/tuscany/tuscanyLobby.js
+++ client/src/tuscany/tuscanyLobby.js
@@ -2,14 +2,15 @@ import { Lobby } from "boardgame.io/react";
 import Tuscany from "./game";
 import Board from "./board";
 import React from "react";
+console.log(process.env);
 
 const TuscanyLobby = () => {
   const style = {};
   return (
     <div style={style}>
       <Lobby
-        gameServer={`http://${window.location.hostname}:8000`}
-        lobbyServer={`http://${window.location.hostname}:8000`}
+        gameServer="http://127.0.0.1:8000"
+        lobbyServer="http://127.0.0.1:8000"
         gameComponents={[{ game: Tuscany, board: Board }]}
         debug={true}
       />
